e05032c124a90bbc0586c39191cb0d8d8c70602b
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-04-09 18:26:58 -0300

    main struct

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"github.com/DevDaniloFerrari/homeworke-done-api/internal/database"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	connectionString := "postgresql://posts"
+	_, err := database.NewConnection(connectionString)
+	if err != nil {
+		panic(err)
+	}
+
 	g := gin.Default()
 	g.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
